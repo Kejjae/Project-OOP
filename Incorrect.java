a413a552e4c132c7c716a65e3ace089abb4e73d8
dreammmmmmmmmmmmmmmmmm <118044738+dreammmmyyyyyyyyyyyyyyyysocute@users.noreply.github.com>
2023-04-28 15:25:36 +0700

    Update Incorrect.java

--- Incorrect.java
+++ Incorrect.java
@@ -4,18 +4,17 @@ import java.awt.*;
 public class Incorrect {
     private JFrame fr;
     private JLabel lb, lb2;
-    private JButton bt;
+    private JButton ok;
     public Incorrect(){
         fr = new JFrame("Ya Luem Khuen");
         lb = new JLabel("                                  X Incorrect!");
         lb2 = new JLabel("                    The username or password");
-        bt = new JButton("OK");
+        ok = new JButton("OK");
         fr.setLayout(new GridLayout(3, 1));
         fr.add(lb); 
         fr.add(lb2);
-        fr.add(bt);
-        fr.setLocation(600, 300);
-        fr.setSize(300, 200);
+        fr.add(pk);
+        fr.setBounds(600, 300, 300, 200);
         fr.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         fr.setVisible(true);
     }
